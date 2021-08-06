46576b7bbda57f96a097554e94fd7fb1e307c4d2
CreeperG16 <pappt1216@gmail.com>
2021-08-06 19:46:17 +0200

    Add Ghosty Set

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 */
 
 // Edit These
-var url = "https://mcstore.io/the-hive/grim-creeper-costume";
-var threeKeywords = "Creeper?, Aww, Man";
+var url = "https://mcstore.io/the-hive/ghosty-costume";
+var threeKeywords = "Ghost, Cute, Boo";
 // Edit These
 
 /*
