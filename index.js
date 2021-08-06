de80b5cfb268bd52b2ef193635a3bedb00169667
CreeperG16 <pappt1216@gmail.com>
2021-08-06 19:24:27 +0200

    Add Rufus Set

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 */
 
 // Edit These
-var url = "https://mcstore.io/the-hive/ice-dragon-costume";
-var threeKeywords = "Ice, Cold, Ginger";
+var url = "https://mcstore.io/the-hive/rufus-costume";
+var threeKeywords = "Dog, Pet, Animal";
 // Edit These
 
 /*
